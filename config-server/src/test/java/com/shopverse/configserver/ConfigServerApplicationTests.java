790b465aef3eb62cade5d39ef346a6024599b6c8
rimac-code <56124534+rimac-code@users.noreply.github.com>
2025-07-23 12:57:10 +1000

    Setting up config server

---
+++ config-server/src/test/java/com/shopverse/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopverse.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
